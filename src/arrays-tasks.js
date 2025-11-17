468890bae95e8ae890a7ab024d61156240754f6e
nohawaheed <nohawaheed.1991@gmail.com>
2025-11-18 00:04:56 +0200

    feat: add getIdentityMatrix implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -417,8 +417,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, rowIndex) => {
+    return Array.from({ length: n }, (__, colIndex) =>
+      rowIndex === colIndex ? 1 : 0
+    );
+  });
 }
 
 /**
